236bc6559db9d487831c08f79da0c019877b3e79
jamoon2030 <jungaamoon@gmail.com>
2020-04-18 13:52:41 -0400

    minor change in legend

--- plot4.R
+++ plot4.R
@@ -35,7 +35,7 @@ plot(data$datetime, data$Voltage, type = "l", xlab = "datetime", ylab = "Voltage
 plot(data$datetime, data$Sub_metering_1, type="l", col="black", lty=1, ylab = "Energy sub metering", xlab = "")
 lines(data$datetime, data$Sub_metering_2, type="l", col="red", lty=1)
 lines(data$datetime, data$Sub_metering_3, type="l", col="blue", lty=1)
-legend("topright", col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty = c(1,1,1))
+legend("topright", col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty = c(1,1,1),box.lty=0,inset=.02)
 
 # bottom-right plot
 plot(data$datetime, data$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
